a1be807a984f654044cc42f538a3aa9ea1709f6f
dusanzigic <zigic.dusan1991@gmail.com>
2026-01-04 22:36:51 +0100

    release 1

--- build.ps1
+++ build.ps1
@@ -1,15 +1,12 @@
-$Compiler    = "g++"
-$Target      = "ks.exe"
-$Source      = Get-ChildItem -Path "src" -Filter *.cpp | ForEach-Object { $_.FullName }
-$IncludePath = "-Iinclude"
-
-$CXXFlags = @(
-    "-std=c++17",
-    "-O2",
-    "-mwindows",
-    "-Wall",
-    "-Wextra"
+param (
+    [switch]$Release
 )
+
+$Compiler    = "g++"
+$IncludePath = "-Iinclude"
+$Source      = Get-ChildItem -Path "src" -Filter *.cpp | ForEach-Object { $_.FullName }
+$ResourceSrc = "resources.rc"
+$ResourceObj = "resources.res"
 
 $LDLibs = @(
     "-lgdi32",
@@ -24,12 +21,43 @@ $LDLibs = @(
     "-pthread"
 )
 
-Write-Host "Building $Target..." -ForegroundColor Cyan
+if ($Release) {
+    $TargetDir = "bin"
+    $Target    = "$TargetDir/ks.exe"
+    if (!(Test-Path $TargetDir)) { New-Item -ItemType Directory -Path $TargetDir }
 
-& $Compiler $Source -o $Target $IncludePath $CXXFlags $LDLibs
+    Write-Host "Building RELEASE Version..." -ForegroundColor Magenta
+
+    & windres $ResourceSrc -O coff -o $ResourceObj
+
+    $CXXFlags = @(
+        "-std=c++17",
+        "-O3",
+        "-mwindows",
+        "-static",
+        "-s",
+        "-Wall"
+    )
+    
+    & $Compiler $Source $ResourceObj -o $Target $IncludePath $CXXFlags $LDLibs
+} 
+else {
+    $Target = "ks.exe"
+    Write-Host "Building DEBUG Version..." -ForegroundColor Cyan
+
+    $CXXFlags = @(
+        "-std=c++17",
+        "-g",
+        "-Wall",
+        "-Wextra"
+    )
+
+    & $Compiler $Source -o $Target $IncludePath $CXXFlags $LDLibs
+}
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "Build Successful!" -ForegroundColor Green
+    if ($ResourceObj -and (Test-Path $ResourceObj)) { Remove-Item $ResourceObj }
 } else {
     Write-Host "Build Failed with exit code $LASTEXITCODE" -ForegroundColor Red
 }
